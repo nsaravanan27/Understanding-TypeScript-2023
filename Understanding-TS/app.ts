2147e83e9a4cb686eba9161a6afaf0f2ed42c9cd
sn <saravanan.n@radisys.com>
2022-12-04 11:13:22 +0530

    Using types add function

--- Understanding-TS/app.ts
+++ Understanding-TS/app.ts
@@ -1,1 +1,11 @@
-console.log('Your code goes here...!'); 
+function add(n1: number, n2: number) {
+    /* if( typeof n1 !== "number" || typeof n2 !== "number") {
+        throw new Error("Invalid types")
+    } */
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 20;
+
+console.log(add(number1, number2));
